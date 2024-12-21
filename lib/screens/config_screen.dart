4ace1dc762cd692a231080361e0e625d3a23bd06
fran <francoRS832@gmail.com>
2024-12-21 10:19:20 -0500

    compartir aplicacion

--- lib/screens/config_screen.dart
+++ lib/screens/config_screen.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
-class ConfigScreen extends StatelessWidget {
-  const ConfigScreen({super.key});
+class ConfigScreen extends StatefulWidget {
+  @override
+  _ConfigScreenState createState() => _ConfigScreenState();
+}
+
+class _ConfigScreenState extends State<ConfigScreen> {
+  Future<void> shareApp() async {
+    try {
+      // Ruta del .apk
+      final ByteData bytes = await rootBundle.load('assets/apk/pilltime.apk');
+      final tempDir = await getTemporaryDirectory();
+      final file = File('${tempDir.path}/pilltime.apk');
+      await file.writeAsBytes(bytes.buffer.asUint8List());
+
+      // Compartir el archivo
+      await Share.shareXFiles(
+        [XFile(file.path)],
+        text: '¡Descarga la app PillTime!',
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error al compartir la app: $e')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Configuración'),
-        backgroundColor: Colors.green,
-      ),
-      body: const Center(
-        child: Text(
-          'Pantalla de Configuración',
-          style: TextStyle(fontSize: 24),
+      appBar: AppBar(title: Text('Configuración')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: shareApp,
+              child: Text('Compartir aplicación'),
+            ),
+          ],
         ),
       ),
     );
